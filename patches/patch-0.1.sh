f79ebe5c8d81ba4460d639f250e33fa43bbb3faa
-S-S- <intellnet@users.noreply.github.com>
2018-02-05 23:53:09 +0100

    Update-patch-0.1 [ cd ~/.smartcash ]
    cd ~/.smartnode ??? --> cd ~/.smartcash

--- patches/patch-0.1.sh
+++ patches/patch-0.1.sh
@@ -1,12 +1,12 @@
 # Change the directory to ~/smartnode
-cd ~/smartnode
+cd ~/.smartcash
 
 # Download the new crontjob
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/clearlog.sh 
 
 # Create a cronjob for clearing the log file
-if ! crontab -l | grep "~/smartnode/clearlog.sh"; then
-  (crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
+if ! crontab -l | grep "~/.smartcash/clearlog.sh"; then
+  (crontab -l ; echo "0 0 */2 * * ~/.smarcash/clearlog.sh") | crontab -
 fi
 
 # Give execute permission to the cron script
